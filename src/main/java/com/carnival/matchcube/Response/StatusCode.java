f1dd29363894f8f35562d429df3807769fe337be
HanInJu <haninju95@gmail.com>
2021-03-07 15:35:22 +0900

    StatusCode 수정

--- src/main/java/com/carnival/matchcube/Response/StatusCode.java
+++ src/main/java/com/carnival/matchcube/Response/StatusCode.java
@@ -1,20 +1,20 @@
 package com.carnival.matchcube.Response;
 
 public class StatusCode {
-    public static final int OK = 200;
-    public static final int CREATED = 201;
-    public static final int NO_EMAIL = 202;
-    public static final int NO_CONTENT = 204;
-    public static final int DUPLICATE_EMAIL = 205;
+    public static final int OK = 100;
+    public static final int CREATED = 101;
+    public static final int NO_EMAIL = 201;
+    public static final int NO_CONTENT = 202;
+    public static final int DUPLICATE_EMAIL = 203;
+    public static final int NO_HAVE_TEAMS = 204;
+    public static final int SIGN_IN_FAIL = 205;
+    public static final int NOT_VALID_AREA_CODE = 206;
+    public static final int DUPLICATE_TEAM_NAME = 207;
+    public static final int NOT_VALID_ACCOUNT_ID = 208;
     public static final int BAD_REQUEST =  400;
     public static final int UNAUTHORIZED = 401;
     public static final int FORBIDDEN = 403;
     public static final int NOT_FOUND = 404;
-    public static final int SIGN_IN_FAIL = 405;
-    public static final int NOT_VALID_AREA_CODE = 406;
-    public static final int DUPLICATE_TEAM_NAME = 408;
-    public static final int NOT_VALID_ACCOUNT_ID = 407;
-    public static final int NO_HAVE_TEAMS = 409;
     public static final int INTERNAL_SERVER_ERROR = 500;
     public static final int SERVICE_UNAVAILABLE = 503;
     public static final int DB_ERROR = 600;
