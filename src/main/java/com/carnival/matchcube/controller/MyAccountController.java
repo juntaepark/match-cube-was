336e6a0c389db100f92e02e9e603dcf45b528e89
iding15 <ekfrhd@hanyang.ac.kr>
2021-03-14 16:26:10 +0900

    jin-myupdate

--- src/main/java/com/carnival/matchcube/controller/MyAccountController.java
+++ src/main/java/com/carnival/matchcube/controller/MyAccountController.java
@@ -1,13 +1,11 @@
 package com.carnival.matchcube.controller;
 
 import com.carnival.matchcube.dto.MyAccountDTO;
+import com.carnival.matchcube.dto.MyUpdateDTO;
 import com.carnival.matchcube.service.MyAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/myAccount")
@@ -24,5 +22,9 @@ public class MyAccountController {
     public ResponseEntity myTeam(@PathVariable("accountId") Long accountId) throws Exception {
         MyAccountDTO myAccountDTO = new MyAccountDTO(accountId);
         return myAccountService.myTeam(myAccountDTO);
+    }
+    @PatchMapping
+    public ResponseEntity myUpdate(MyUpdateDTO myUpdateDTO) throws Exception {
+        return myAccountService.myUpdate(myUpdateDTO);
     }
 }
